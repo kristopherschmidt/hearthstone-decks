5ad3d3822b3a3e4f312f749deb52c7fa81e2d6f3
Kristopher Schmidt <kris.schmidt7@gmail.com>
2015-07-03 11:53:12 -0400

    add search to ui

--- src/main/java/com/kschmidt/hearthstone/web/rest/DeckDiffResource.java
+++ src/main/java/com/kschmidt/hearthstone/web/rest/DeckDiffResource.java
@@ -1,12 +1,14 @@
 package com.kschmidt.hearthstone.web.rest;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kschmidt.hearthstone.domain.Deck;
@@ -28,9 +30,28 @@ public class DeckDiffResource {
 	}
 
 	@RequestMapping(value = "/api/diffs2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<DeckDiff> diffs2() throws IOException {
-		return DeckDiff.diffDecks(userDeck,
-				mongoDeckRepository.findDecksContainingCard("Ragnaros the Firelord"));
+	public List<DeckDiff> diffs2(@RequestParam(required = false, value = "card") String cardName) throws IOException {
+		return DeckDiff.diffDecks(userDeck, mongoDeckRepository
+				.findDecksContainingCard(cardName));
 	}
 
+	@RequestMapping(value = "/api/diffs3", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<DeckDiff> diffs3() throws IOException {
+		return DeckDiff.diffDecks(userDeck, mongoDeckRepository
+				.findDecksContainingAllCards(Arrays.asList("Dr. Boom",
+						"Sylvanas Windrunner")));
+	}
+	
+	@RequestMapping(value = "/api/diffs4", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<DeckDiff> diffs4() throws IOException {
+		return DeckDiff.diffDecks(userDeck, mongoDeckRepository
+				.findDecksContainingAllCards(Arrays.asList("Dr. Boom",
+						"Sylvanas Windrunner", "Ragnaros the Firelord")));
+	}
+	
+	@RequestMapping(value = "/api/diffs5", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<DeckDiff> diffs5() throws IOException {
+		return DeckDiff.diffDecks(userDeck, mongoDeckRepository
+				.findDecksContainingAllCards(Arrays.asList("Sea Giant")));
+	}
 }
--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -2,11 +2,27 @@ angular.module('hearthstoneApp', []).controller('IndexController',
 		[ '$scope', '$http', function($scope, $http) {
 
 			$scope.diffAll = function() {
-				$http.get('/api/diffs2').success(function(data) {
+				$http.get('/api/diffs', {
+					params: {
+						card: $scope.card
+					}
+				}).success(function(data) {
+					$scope.diffs = data;
+				})
+			}
+			
+			$scope.search = function() {
+				$http.get('/api/diffs2', {
+					params: {
+						card: $scope.card
+					}
+				}).success(function(data) {
 					$scope.diffs = data;
 				})
 			}
 			
 			$scope.diffAll();
+			
+
 
 		} ]);
