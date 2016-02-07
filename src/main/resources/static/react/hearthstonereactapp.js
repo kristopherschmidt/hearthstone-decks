6682f83c0034581210421830e33525e999b3ebc3
Kris Schmidt <kris.schmidt7@gmail.com>
2016-02-07 10:56:12 -0500

    begin to browserify

--- src/main/resources/static/react/hearthstonereactapp.js
+++ src/main/resources/static/react/hearthstonereactapp.js
@@ -1,3 +1,27 @@
+var adminMenuItemData = [ { link : "#/admin/load", text : "Load Decks" } ];
+var collectionMenuItemData = [ 
+	{ text : "HearthPwn", link : "http://hearthpwn.com" },
+	{ text : "Hearthstone Top Deck", link : "http://hearthstonetopdeck.com" },
+	{ text : "IcyVeins", link : "http://www.icy-veins.com/hearthstone" },
+	{ text : "Tempo Storm", link : "https://tempostorm.com/hearthstone/decks"}
+];
+var adminMenuData = { icon : 'icon_tools', text : "Admin", menuItemData : adminMenuItemData }
+var collectionMenuData = { icon : 'icon_archive_alt', text : "Collections", menuItemData : collectionMenuItemData }
+var menuData = [ adminMenuData, collectionMenuData ];
+
+ReactDOM.render(
+		<Sidebar homeLink="/index_react.html" menuData={menuData} />,
+		document.getElementById('sidebar-wrapper')
+);
+
+
+
+
+
+
+
+
+
 var CommentBox = React.createClass({
 	render: function() {
 		return (
---
+++ src/main/resources/static/react/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link } from 'react-router'
+
+var Hi = React.createClass({
+
+	render: function() {
+		return (
+			<h1>Hello</h1>
+		);
+	}
+
+})
+
+
+
